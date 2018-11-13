1989eba7ce306cafde282085164c314744ec9669
xuhongwei5 <xuhongwei5@jd.com>
2018-11-13 16:36:48 +0800

    guess number

---
+++ guess_number/src/main.rs
@@ -0,0 +1,34 @@
+extern crate rand;
+
+use std::cmp::Ordering;
+use std::io;
+
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!");
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => {
+                println!("Wow ~ You guessed it!");
+                return;
+            }
+            Ordering::Greater => println!("Too big!"),
+        }
+    }
+}
